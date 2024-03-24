e6e06c3b93edd3b071bc7b95a71225c1d4ebc6fc
Mark Eissler <mark@bunker5.com>
2024-03-24 15:51:36 -0700

    Fix download URL for ddclient-ubnt in update script.

--- ddclient-ubnt/config/scripts/firstboot.d/ddclient-ubnt.sh
+++ ddclient-ubnt/config/scripts/firstboot.d/ddclient-ubnt.sh
@@ -7,5 +7,5 @@
 set -e
 
 sudo cp /usr/sbin/ddclient-ubnt /usr/sbin/ddclient-ubnt.dist
-sudo curl -sSL -o /usr/sbin/ddclient-ubnt https://raw.githubusercontent.com/markeissler/edgeos-extras/master/ddclient/dist/ddclient-ubnt
+sudo curl -sSL -o /usr/sbin/ddclient-ubnt https://raw.githubusercontent.com/markeissler/edgeos-extras/main/ddclient-ubnt/dist/ddclient-ubnt
 chmod 0755 /usr/sbin/ddclient-ubnt
